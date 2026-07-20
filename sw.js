8133678b71d65fe63b9f80e84da9eb260291c7bb
nexuslabskoji <ninjaramen@icloud.com>
2026-07-20 14:03:43 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='kenko-karte-v144-no-cache';
+const CACHE_NAME='kenko-karte-v1441-no-cache';
 self.addEventListener('install',event=>{self.skipWaiting();});
 self.addEventListener('activate',event=>{
   event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
